60fbfff6059b7aa9adca08ac872f296de7d56393
Lee Katie <161503286+Superkatielee@users.noreply.github.com>
2024-04-17 09:41:25 -0700

    Create bot_katie1.py

---
+++ bot_katie1.py
@@ -0,0 +1,7 @@
+def choice (round_score, my score, opponent score):
+       if my_score + round_score <= 100:
+            return True
+        else:
+            return False
+        else:
+        return opponent_score > my_scores + opponent_score or my_score < 20
